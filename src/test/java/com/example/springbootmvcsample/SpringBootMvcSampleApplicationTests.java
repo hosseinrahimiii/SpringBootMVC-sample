30f00025fe19201c022040c113767fc61e5d52bb
hosseinrahimiii <hosseinrahimi.dev@gmail.com>
2022-05-31 11:02:39 +0430

    init

---
+++ src/test/java/com/example/springbootmvcsample/SpringBootMvcSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmvcsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
